2aa43b1760d3ece6347cd515de53ca5a823f917b
Sergey <sergeyxyzz@yandex.ru>
2022-07-27 05:58:16 +0300

    add usememo

--- src/App.js
+++ src/App.js
@@ -1,59 +1,6 @@
-import {Component, useState, useEffect, useCallback} from 'react';
+import {Component, useState, useEffect, useCallback, useMemo} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
-
-// class Slider extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
-
-//     componentDidMount() {
-//         document.title = `Slide: ${this.state.slide}`
-//     }
-
-//     componentDidUpdate() {
-//         document.title = `Slide: ${this.state.slide}`
-//     }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
 
 
 
@@ -63,6 +10,11 @@ const Slider = (props) => {
 
     function logging() {
         console.log('log!')
+    }
+
+    const countTotal = (num) => {
+        console.log('counting')
+        return num + 10
     }
 
     useEffect(() => {
@@ -75,10 +27,6 @@ const Slider = (props) => {
             window.removeEventListener('click', logging)
         }
     }, [slide]) 
-
-    useEffect(() => {
-        console.log('autoplay')
-    }, [autoplay])
 
     const getSomeImages = useCallback(() => {
         console.log('fetching')
@@ -94,24 +42,28 @@ const Slider = (props) => {
         setAutoplay(autoplay => !autoplay)
     }
 
+    const total = useMemo(() => {
+        return countTotal(slide)
+    }, [slide])
+
+    const style = useMemo(() => ({
+        color: slide > 4 ? 'red' : 'black'
+    }), [slide])
+
+    useEffect(() => {
+        console.log('style')
+    }, [style]) 
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-
-                {/* {
-                    getSomeImages().map((url, i) => {
-                        return (
-                            <img key={i} className="d-block w-100" src={url} alt="slide" />
-                        )
-                    })
-                } */}
-
                 <Slide getSomeImages={getSomeImages}/> 
 
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                 {autoplay ? 'auto' : null}
                 </div>
+                <div style={style} className='text-center mt-5'>Total slides: {total}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
